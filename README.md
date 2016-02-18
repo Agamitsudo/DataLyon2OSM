5b0224185ba679675193bd069c5a348576c73a60
agamitsudo <agamitsudo@gmail.com>
2016-02-18 22:49:03 +0100

    update json sample

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 == DataLyon2OSM ==
 * Displays every geolocalized dataset (from Lyon administration) on Umap.
 * Provides a gpx converter for JOSM edition.
+* http://agamitsudo.ovh/DataLyon2OSM/
 
--- main.js
+++ main.js
@@ -3,27 +3,23 @@ $(document).ready(function() {
         $('#main').append("<select id='setData' /><br><br>");
   	$('#main').hide().fadeIn(500);
 
-	var maps = {
-		"Sélectionnez un jeu de données": "0",
-		"Stations Vélov (disponibilité temps réel)":"165923",
-  		"Aménagement cyclable": "165928",
- 		"Tronçon de la trame viaire (voies et adresses)": "166200",
-		"Point d'intérêt touristique": "166231",
-		"Commune du Grand Lyon (voies et adresses)":"166233"
-	};
+	var url = "http:/www.agamitsudo.ovh/DataLyon2OSM/data.json";
 
-	$.each(maps, function( key, value ) {
+	getJSON(url).then(function(data) {
 
-		$('#setData').append($('<option>', {
-    			value: value,
-    			text: key
-		}));	
+		for(var i=0;i<data.length; i++){
+		    element = data[i];
+		    $('#setData').append($('<option>', {
+	    		value: element.id,
+	    		text: element.name
+		    }));	
+		}
 	});
 
 	$("#setData").change(function() {
 
 
-	var iframe_map = '<br><iframe width="99%" height="99%" frameBorder="0" src="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781?datalayers=' +  $("#setData option:selected").val() + '&scaleControl=false&miniMap=true&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781">Voir en plein écran</a></p>';
+	var iframe_map = '<br><iframe width="75%" height="75%" frameBorder="0" src="https://umap.openstreetmap.fr/fr/map/datalyon2osm_72010?datalayers=' +  $("#setData option:selected").val() + '&scaleControl=false&miniMap=true&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781">Voir en plein écran</a></p>';
 
 	//alert (iframe_map);
 
@@ -31,3 +27,23 @@ $(document).ready(function() {
  	   $('#iframe').append(iframe_map);
 	});
 });
+
+//
+// getJSON
+//
+var getJSON = function(url) {
+  return new Promise(function(resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('get', url, true);
+    xhr.responseType = 'json';
+    xhr.onload = function() {
+      var status = xhr.status;
+      if (status == 200) {
+        resolve(xhr.response);
+      } else {
+        reject(status);
+      }
+    };
+    xhr.send();
+  });
+};
