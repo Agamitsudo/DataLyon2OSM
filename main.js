ba7d4219f065259e69aa34ebb7127b8ac001556d
agamitsudo <agamitsudo@gmail.com>
2016-02-18 18:10:54 +0100

    first version online

--- main.js
+++ main.js
@@ -6,7 +6,10 @@ $(document).ready(function() {
 	var maps = {
 		"Sélectionnez un jeu de données": "0",
 		"Stations Vélov (disponibilité temps réel)":"165923",
-  		"Aménagement cyclable": "165928"
+  		"Aménagement cyclable": "165928",
+ 		"Tronçon de la trame viaire (voies et adresses)": "166200",
+		"Point d'intérêt touristique": "166231",
+		"Commune du Grand Lyon (voies et adresses)":"166233"
 	};
 
 	$.each(maps, function( key, value ) {
@@ -20,7 +23,7 @@ $(document).ready(function() {
 	$("#setData").change(function() {
 
 
-	var iframe_map = '<iframe width="100%" height="50%" frameBorder="0" src="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781?datalayers=' +  $("#setData option:selected").val() + '&scaleControl=false&miniMap=false&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=true&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781">Voir en plein écran</a></p>';
+	var iframe_map = '<br><iframe width="99%" height="99%" frameBorder="0" src="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781?datalayers=' +  $("#setData option:selected").val() + '&scaleControl=false&miniMap=true&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781">Voir en plein écran</a></p>';
 
 	//alert (iframe_map);
 
