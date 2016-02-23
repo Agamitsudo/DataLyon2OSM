621ef133420831549ca53f131aead30df18da3ce
agamitsudo <agamitsudo@gmail.com>
2016-02-24 00:31:23 +0100

    Export ok for Point

--- main.js
+++ main.js
@@ -29,7 +29,10 @@ $(document).ready(function() {
 		    }
 	   }
 
-	   var iframe_map = '<br><iframe width="600px" height="400px" frameBorder="0" src="https://umap.openstreetmap.fr/fr/map/datalyon2osm_72010?datalayers=' +  $("#setData option:selected").val() + '&scaleControl=false&miniMap=true&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781" target="_blank">Voir en plein écran</a></p>'; // &maxfeatures=30
+           //
+           // Sample 30 items
+           //
+	   var iframe_map = '<br><iframe width="600px" height="300px" frameBorder="0" src="https://umap.openstreetmap.fr/fr/map/datalyon2osm_72010?datalayers=' +  $("#setData option:selected").val() + '&scaleControl=false&miniMap=true&scrollWheelZoom=false&zoomControl=true&allowEdit=false&moreControl=false&datalayersControl=false&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="https://umap.openstreetmap.fr/fr/map/datalyon2osm_71781" target="_blank">Voir en plein écran</a></p>'; // &maxfeatures=30
 
 	   $('#iframe').empty();
 	   $('#iframetitle').empty();
@@ -37,26 +40,98 @@ $(document).ready(function() {
  	   $('#iframe').append(iframe_map);
 	   $('#iframe').append('<p><a href="' + element.url + '" target="_blank">Descriptif du jeu de données</a></p>');
 
+
+           //
+	   // Génération d'un fichier .osm
+           //
 	   $('#osm').empty();	
 	   $('#osmtitle').empty();
-	   $('#osmtitle').append("<h2>2. Génération d'un fichier .osm</h2>");
-           
-	   $.getJSON( element.sample, function(data) {
+	   $('#osmtitle').append("<h1>2. Génération d'un fichier .osm</h1>");
+           $('#osmtitle').append("<label>Générer le fichier .osm pour l'ensemble des données disponibles </label><button id='gen'>Générer</button><br/>");
 
-		
-		   var table = $('<table></table>');
-		   var row = $('<tr></tr>');
-		   var rowdata = "<td>propriété json</td><td>exemple de valorisation</td><td>correspondance osm</td><td>notes</td>";
-		   row.append(rowdata);
-		   table.append(row);
+	   $('#gen').click(function() {
 
-		   $.each( data.features[0].properties, function(key, val) {
-		      	   row = $('<tr></tr>');		
-	    	      	   rowdata = "<td>" + key + "</td><td>" + val + "</td><td><input type='text' size='50' id='" + key + "'></td><td>...</td>";
-		           row.append(rowdata);		
-			   table.append(row);	
-		   }); 
-		   $('#osm').append(table);
-        });
+		   $("#setData").attr("disabled", true); 
+		   $("#gen").attr("disabled", true); 
+
+		   $('#iframe').empty();
+	           $('#iframetitle').empty();	
+		   $('#osm').empty();	
+
+		   $.getJSON( element.sample, function(data) {
+			   
+		           /*	
+			   var table = $('<table></table>');
+			   var row = $('<tr></tr>');
+			   var rowdata = "<td>propriété json</td><td>exemple de valorisation</td><td>correspondance osm</td><td>notes</td>";
+			   row.append(rowdata);
+			   table.append(row);
+
+			   $.each( data.features[0].properties, function(key, val) {
+			      	   row = $('<tr></tr>');		
+		    	      	   rowdata = "<td>" + key + "</td><td>" + val + "</td><td><input type='text' size='50' id='" + key + "'></td><td>...</td>";
+				   row.append(rowdata);		
+				   table.append(row);	
+			   }); 
+			   $('#osm').append(table);
+			   */
+
+			   // Point / LineString / Polygon ?
+			   // "geometry": { "type": "Point", "coordinates": [ 4.845192520164354, 45.750263873691459 ] } },
+			   // "geometry": { "type": "LineString", "coordinates": [ [ 4.913027724848727, 45.670199714539663 ], 
+			   // "Polygon", "coordinates": [ [ [ 4.790407945472112, 45.869014690473236 ], [ 4.789913246646215, 45.869051348990531 ]
+			   var type = data.features[0].geometry.type;
+			   
+			   if (type == "Point")
+			   {
+				$.getJSON( element.sample, function(data) {
+				
+					var cmp = 1;
+
+				        var input = '<?xml version="1.0" encoding="UTF-8"?>\r\n';
+				        input += '<osm version="0.6" upload="true" generator="JOSM">\r\n';
+				
+	 				for(var i=0;i<data.features.length; i++){	
+					
+						input += '<node action="modify" visible="true" id="-'  + cmp.toString() + '" lat="' + data.features[i].geometry.coordinates[0] + '" lon="' + data.features[i].geometry.coordinates[1] + '">\r\n';
+					
+						cmp++;
+
+						$.each( data.features[i].properties, function(key, val) {
+							var myval = val.replace(/>/g, "+");
+							var myval = myval.replace(/</g, "-");
+							var myval = myval.replace(/"/g, "");
+
+							input += '<tag k="' + key + '" v="' + myval + '"/>\r\n';
+						}); 
+						input += "</node>\r\n";
+					}
+					input += '</osm>\r\n';
+					input = input.replace(/&/g, "et");
+
+				   	$("#setData").attr("disabled", false); 
+	  				$("#gen").attr("disabled", false); 
+					$('#osm').append("<label>" + data.features.length.toString() + " éléments traités </label><button id='btn-save'>Téléchargez le fichier .osm</button><br/><br/>");
+					
+					$("#btn-save").click( function() {
+					
+					  var text = input;
+					  var filename = "output";
+					  var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
+					  saveAs(blob, filename+".osm");
+					});
+	
+					
+			   	   });
+		           }
+			   else if (type == "LineString")
+			   {
+		           }
+			   else if (type == "Polygon")
+			   {
+		           }
+			   
+		});
+	  });
     });
 });
